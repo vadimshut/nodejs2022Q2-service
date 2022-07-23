fe123ca0851d86cba5541c91785b60bde11cc24b
Vadzim Shut <vadzim_shut@epam.com>
2022-07-24 02:25:07 +0300

    feat: update albums module

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,13 +1,19 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesModule } from 'src/favorites/favorites.module';
 import { TracksModule } from 'src/tracks/tracks.module';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
+import { AlbumEntity } from './entities/album.entity';
 
 @Module({
   providers: [AlbumsService],
   controllers: [AlbumsController],
-  imports: [forwardRef(() => TracksModule), forwardRef(() => FavoritesModule)],
+  imports: [
+    forwardRef(() => TracksModule),
+    forwardRef(() => FavoritesModule),
+    TypeOrmModule.forFeature([AlbumEntity]),
+  ],
   exports: [AlbumsService],
 })
 export class AlbumsModule {}
---
+++ src/albums/entities/album.entity.ts
@@ -0,0 +1,4 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('album')
+export class AlbumEntity {}
