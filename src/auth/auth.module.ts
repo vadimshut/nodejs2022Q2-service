18b300b753a053130ed37530920d7122cba00e39
Vadzim Shut <vadzim_shut@epam.com>
2022-08-04 23:58:33 +0300

    feat: implement basic auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { AuthStrategy } from './strategies/authToken.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [JwtModule.register({}), UsersModule],
+  controllers: [AuthController],
   providers: [AuthService, AuthStrategy],
 })
 export class AuthModule {}
