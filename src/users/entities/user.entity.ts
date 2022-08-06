1094bddc0780a22b3e4997b46f6793c89e818cec
Vadzim Shut <vadzim_shut@epam.com>
2022-08-06 21:42:49 +0300

    fix: update readme

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -25,7 +25,7 @@ export class UserEntity implements IUser {
   hashRefreshToken: string | null;
 
   toResponse() {
-    const { password: _password, ...response } = this;
+    const { password, ...response } = this;
     return response;
   }
 }
